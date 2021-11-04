3dc5b8b60200d9fe3e4394c2f7bbbcd7521fc1c1
Tu Pham <toopham@gmail.com>
2021-11-03 21:46:25 -0700

    upload file button

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -1,14 +1,37 @@
+import { useState, useEffect } from 'react';
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
 
-const Hello = () => {
+const Home = () => {
+  const [state, setState] = useState({
+    rootDir: '',
+  });
+
+  const dirButtonValue = 'Set Root Directory';
+  dirDisplay = (e) => {
+    let dir = document.getElementById('dir-upload').files[0].path;
+		const name = document.getElementById('dir-upload').files[0].name;
+		dir = dir.replace(name, '');
+    setState({ rootDir: dir });
+    console.log('DIRECTORY = ', name);
+  };
+
   return (
     <div>
       <div className="Hello">
         <img width="200px" alt="icon" src={icon} />
       </div>
       <h1>electron-react-boilerplate</h1>
+      <label htmlFor="dir-upload" className="custom-file-upload">
+        {state.rootDir || 'Set Root Directory'}
+      </label>
+      <input
+        type="file"
+        webkitdirectory="true"
+        id="dir-upload"
+        onChange={(e) => dirDisplay(e)}
+      />
       <div className="Hello">
         <a
           href="https://electron-react-boilerplate.js.org/"
@@ -43,7 +66,7 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Route path="/" component={Home} />
       </Switch>
     </Router>
   );
